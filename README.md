b1120c34cf39de94acd7197706b243df6f5b78c1
unknown <t6o.mrc@gmail.com>
2018-05-22 17:08:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+npm install -g @angular/cli
+ng new vittoriaAssicurazioni
+cd vittoriaAssicurazioni
+ng serve --open
+
+
+npm install --save @angular/material @angular/cdk
+npm install --save @angular/animations
+
+- Import the component modules
+- Whichever approach you use, be sure to import the Angular Material modules after Angular's BrowserModule, as the import order matters for NgModules.
+- theme
+  If you're using the Angular CLI, you can add this to your styles.css
+  If you are not using the Angular CLI, you can include a prebuilt theme via a <link> element in your index.html.
+
+
+COMPONENT
+https://material.angular.io/components/select/overview
+
+
+LAYOUT
+https://material.angularjs.org/latest/layout/container
---
+++ src/app/app.module.ts
@@ -0,0 +1,105 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+
+} from '@angular/material';
+
+const appRoutes: Routes = [
+
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatAutocompleteModule,
+   MatBadgeModule,
+   MatBottomSheetModule,
+   MatButtonModule,
+   MatButtonToggleModule,
+   MatCardModule,
+   MatCheckboxModule,
+   MatChipsModule,
+   MatDatepickerModule,
+   MatDialogModule,
+   MatDividerModule,
+   MatExpansionModule,
+   MatGridListModule,
+   MatIconModule,
+   MatInputModule,
+   MatListModule,
+   MatMenuModule,
+   MatNativeDateModule,
+   MatPaginatorModule,
+   MatProgressBarModule,
+   MatProgressSpinnerModule,
+   MatRadioModule,
+   MatRippleModule,
+   MatSelectModule,
+   MatSidenavModule,
+   MatSliderModule,
+   MatSlideToggleModule,
+   MatSnackBarModule,
+   MatSortModule,
+   MatStepperModule,
+   MatTableModule,
+   MatTabsModule,
+   MatToolbarModule,
+   MatTooltipModule,
+   MatTreeModule,
+   RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+
+
+
+export class AppModule { }
